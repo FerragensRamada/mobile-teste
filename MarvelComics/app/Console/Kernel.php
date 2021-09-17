88e9a2059244f1ba43913162058817a46d08f620
Davi de Sousa <deivede@gmail.com>
2021-09-17 00:16:01 -0300

    Readd command

--- MarvelComics/app/Console/Kernel.php
+++ MarvelComics/app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Console\Commands\weekcomics;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,6 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
+      weekcomics::class,
     ];
 
     /**
@@ -23,6 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+      $schedule->command(command: 'send:weekcomics')->weekly();
     }
 
     /**
